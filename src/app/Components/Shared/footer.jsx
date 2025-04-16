ab2357acbc24c7dc649a054e97b6c72fcad9f5e8
Abid Hasan <89175222+Abid191@users.noreply.github.com>
2025-04-16 22:27:35 +0600

    footer added

---
+++ src/app/Components/Shared/footer.jsx
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer className="footer bg-slate-700 text-slate-50 p-10">
+            <aside>
+                <Image alt='logo' src='/assets/logo.svg' width={60} height={50}></Image>
+                <p>
+                    ACME Industries Ltd.
+                    <br />
+                    Providing reliable tech since 2024
+                </p>
+            </aside>
+            <nav>
+                <h6 className="footer-title">Services</h6>
+                <a className="link link-hover">Branding</a>
+                <a className="link link-hover">Design</a>
+                <a className="link link-hover">Marketing</a>
+                <a className="link link-hover">Advertisement</a>
+            </nav>
+            <nav>
+                <h6 className="footer-title">Company</h6>
+                <a className="link link-hover">About us</a>
+                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Jobs</a>
+                <a className="link link-hover">Press kit</a>
+            </nav>
+            <nav>
+                <h6 className="footer-title">Legal</h6>
+                <a className="link link-hover">Terms of use</a>
+                <a className="link link-hover">Privacy policy</a>
+                <a className="link link-hover">Cookie policy</a>
+            </nav>
+        </footer>
+    )
+}
